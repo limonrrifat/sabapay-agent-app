1c9cb2f1de3852feea49300fd3978a8389a23a86
limonrrifat <rifatrlimon@gmail.com>
2025-08-06 15:05:29 +0600

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,7 @@
+export const BASE_URL = "https://pay.sl0610.site/agent/api";
 
+export const API_ENDPOINTS = {
+  login: `${BASE_URL}/login`,
+  dashboard: `${BASE_URL}/dashboard`,
+  // future: add more here
+};

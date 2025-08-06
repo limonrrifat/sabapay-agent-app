123bbaace779a7c7503913c5c6fdd760d249182b
limonrrifat <rifatrlimon@gmail.com>
2025-08-06 21:14:13 +0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/screens/LoginScreen';
-
-const Stack = createNativeStackNavigator();
+import { FitnessContext } from './Context';
+import StackNavigator from './StackNavigator';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+    <FitnessContext>
+        <StatusBar style="light" backgroundColor='#000' />
+        <StackNavigator />
+    </FitnessContext>
+    </>
   );
 }
